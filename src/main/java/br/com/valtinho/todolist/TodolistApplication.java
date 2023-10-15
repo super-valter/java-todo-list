93522458b90e57879652a1e933769e8dbf5ca2ac
super-valter <valter@valtinho.com.br>
2023-10-14 23:45:30 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/valtinho/todolist/TodolistApplication.java
@@ -0,0 +1,16 @@
+package br.com.valtinho.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+	/* 
+	 * mvn spring-boot:run
+	 */
+
+}
---
+++ src/test/java/br/com/valtinho/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valtinho.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
